9383aee0382ffea6e1f7944983f83bbe16ce3b32
gabrielkatsnelson <gbrl.ka@gmail.com>
2014-01-16 22:24:58 -0500

    final commit

---
+++ people.rb
@@ -0,0 +1,18 @@
+class Person
+	def initialize(name) 
+		@name = name 
+		puts "Hi, my name is #{name}"
+	end
+end
+
+class Student < Person
+	def learn
+		puts "I get it!"
+	end
+end
+
+class Instructor < Person 
+	def teach 
+		puts "Everything in Ruby is an Object"
+	end
+end
